0089f007f4d095ba0573318fdda870012e33ade2
bincomNita <nitakalukhe.bincom@gmail.com>
2025-01-09 19:10:05 +0530

    Add project files.

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movie.Models;
+
+namespace Movie.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IEnumerable<MovieItem> TheMovies = [
+            new MovieItem{ Title="Titnanic",Description="description",ReleaseDate=new DateOnly(1999,5,22),Rating=9,Price=78},
+            new MovieItem{ Title="Pani",Description="description",ReleaseDate=new DateOnly(2025,5,22),Rating=9,Price=85}
+            ];
+        public IActionResult GetMovies()
+        {
+            return View(TheMovies);
+        }
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Movie.Models
+{
+    public class MovieItem
+    {
+       // [Required]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateOnly ReleaseDate { get; set; }
+        public int Rating { get; set; }
+        public decimal Price { get; set; }
+    }
+}
